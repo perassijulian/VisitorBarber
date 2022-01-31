fc796604c35d0aff903e1eb1e6a3b10a2d9802e8
Julian Perassi <perassijulian@gmail.com>
2022-01-31 01:52:05 -0300

    finish landing maqueta

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
-import lupa from '../pictures/lupa.png'
+import lupa from '../pictures/lupa.png';
+import background from '../pictures/barber-background.jpg';
 import '../styles/Landing.scss';
 
 const Landing = () => {
@@ -20,7 +21,8 @@ const Landing = () => {
 
   return (
       <div className='landing'>
-        <section>
+        <div className='landing--1'>
+            <h1>dale más calidad de vida a tu pelo, aún cuando no estás a su lado</h1>
             <div className='button--toggle'>
                 <button className={barberoClassName} onClick={handleBarberoClick} >BARBERO</button>
                 <button className={peluqueroClassName} onClick={handlePeluqueroClick}>PELUQUERO</button>
@@ -34,11 +36,13 @@ const Landing = () => {
                     
                 ><img src={lupa} alt="search button"/></button>
             </div>
-        </section>
-        <section>
+        </div>
+        <div className='landing--2'>
             <h1>¿Le estás dando a tu pelo lo que realmente necesita?</h1>
             <h2>El corte diario es una actividad fundamental para la calidad de vida de tu pelo</h2>
-            <button>Buscar peluquero</button>
+            <div className='landing--2--button'>
+                <button className='section--button'>Buscar peluquero</button>
+            </div>
             <div className='container--pros'>
                 <div className='container--pros--item'>
                     <div className='container--pros--item--cercle'>
@@ -46,11 +50,74 @@ const Landing = () => {
                     </div>
                     <div className='container--pros--item--text'>
                         <h4>Beneficio titulo</h4>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna al fuera tuyo</p>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod 
+                        tempor incididunt ut labore et dolore magna al fuera tuyo</p>
+                    </div>
+                </div>
+                <div className='container--pros--item'>
+                    <div className='container--pros--item--cercle'>
+                        <p>🤗</p>
+                    </div>
+                    <div className='container--pros--item--text'>
+                        <h4>Beneficio titulo</h4>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod 
+                        tempor incididunt ut labore et dolore magna al fuera tuyo</p>
+                    </div>
+                </div>
+                <div className='container--pros--item'>
+                    <div className='container--pros--item--cercle'>
+                        <p>🤗</p>
+                    </div>
+                    <div className='container--pros--item--text'>
+                        <h4>Beneficio titulo</h4>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod 
+                        tempor incididunt ut labore et dolore magna al fuera tuyo</p>
                     </div>
                 </div>
             </div>
-        </section>
+        </div>
+        <div className='background--image'>
+        </div>
+        <div className='landing--2'>
+            <h1>¿Le estás dando a tu pelo lo que realmente necesita?</h1>
+            <h2>El corte diario es una actividad fundamental para la calidad de vida de tu pelo</h2>
+            <div className='landing--2--button'>
+                <button className='section--button'>Buscar barbero</button>
+            </div>
+            <div className='container--pros'>
+                <div className='container--pros--item'>
+                    <div className='container--pros--item--cercle'>
+                        <p>🤗</p>
+                    </div>
+                    <div className='container--pros--item--text'>
+                        <h4>Beneficio titulo</h4>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod 
+                        tempor incididunt ut labore et dolore magna al fuera tuyo</p>
+                    </div>
+                </div>
+                <div className='container--pros--item'>
+                    <div className='container--pros--item--cercle'>
+                        <p>🤗</p>
+                    </div>
+                    <div className='container--pros--item--text'>
+                        <h4>Beneficio titulo</h4>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod 
+                        tempor incididunt ut labore et dolore magna al fuera tuyo</p>
+                    </div>
+                </div>
+                <div className='container--pros--item'>
+                    <div className='container--pros--item--cercle'>
+                        <p>🤗</p>
+                    </div>
+                    <div className='container--pros--item--text'>
+                        <h4>Beneficio titulo</h4>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod 
+                        tempor incididunt ut labore et dolore magna al fuera tuyo</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        
       </div>);
 };
 
