ada27efc7e2041631c555a358cb07a90912ab6b8
Julian Perassi <perassijulian@gmail.com>
2022-02-22 15:40:13 -0300

    add routes

--- src/App.js
+++ src/App.js
@@ -8,6 +8,15 @@ import PickDate from './components/PickDate';
 import Landing from './components/Landing';
 import { useSelector } from 'react-redux';
 
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
+import MyAccount from './components/MyAccount';
+
+
 function App() {
   const [showLogin, setShowLogin] = useState(false);
   const [showAddWorker, setShowAddWorker] = useState(false);
@@ -16,27 +25,38 @@ function App() {
   const Auth = useSelector(state => state.Auth);
 
   return (
+    <Router>
     <div className="App">
-      <Navbar
+      
+    <Navbar
         Auth={Auth} 
         setShowLogin={setShowLogin} 
         showLogin={showLogin}
         setShowAddWorker= {setShowAddWorker}
         showAddWorker={showAddWorker}
         setShowLanding={setShowLanding}
-
       />
-      {showLogin && 
-        <Login 
-          Auth={Auth} 
-          setShowLogin={setShowLogin}
-          setShowAddWorker= {setShowAddWorker}
-          showAddWorker={showAddWorker}
-      />}
-      {showAddWorker && <AddWorker setShowAddWorker={setShowAddWorker} />}
-      {showLanding && <Landing setShowLanding={setShowLanding} />}
-      <Main showLogin={showLogin} />
+      <Routes>
+      <Route path='/' element={
+        <div>
+          {showLogin && 
+            <Login 
+              Auth={Auth} 
+              setShowLogin={setShowLogin}
+              setShowAddWorker= {setShowAddWorker}
+              showAddWorker={showAddWorker}
+          />}
+          {showAddWorker && <AddWorker setShowAddWorker={setShowAddWorker} />}
+          {showLanding && <Landing setShowLanding={setShowLanding} />}
+          <Main showLogin={showLogin} />
+        </div>
+      } /> 
+        
+        <Route path='/my-account' element={<MyAccount />} />
+      </Routes>
+
     </div>
+    </Router>
   );
 }
 
