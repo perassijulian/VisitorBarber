b19c7306060e5ca446707f30783e412d6f529ff1
Julian Perassi <perassijulian@gmail.com>
2022-03-03 11:14:04 -0300

    add logout redux

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,38 +1,31 @@
 import React from 'react';
-import { useDispatch } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { 
-    BrowserRouter as Router,
     Link,  
-    Outlet
+    Outlet,
+    useNavigate,
 } from 'react-router-dom';
-import { logoutUser } from '../actions/users';
+import { logout, reset } from '../features/auth/authSlice';
 import '../styles/Navbar.scss';
 
 const Navbar = (props) => {
+    const navigate = useNavigate();
     const dispatch = useDispatch();
+    const { user } = useSelector(state => state.auth);
 
-    const openLogin = () => {
-        props.setShowAddWorker(false);
-        props.setShowLogin(!props.showLogin);
-    }
-
-    const openAddWorker = () => {
-        props.setShowLogin(false);
-        props.setShowAddWorker(!props.showAddWorker);
-    }
-
-    const logOut = () => {
-        dispatch(logoutUser());
-        alert('See you soon 😀')
+    const handleLogout = () => {
+        dispatch(logout());
+        dispatch(reset());
+        navigate('/');
     }
 
   return (
     <div className='navbar'>
         <Link className='navbar--logo' to='/'>VISITOR BARBER</Link>
         <nav className='navbar--nav'>
-             <Link to='/user/login'>LOG IN</Link>
-             <Link to='/user/signup'>REGISTRATE</Link>
-             <Link to='/'>LOG OUT</Link>
+            {!user && <Link to='/user/login'>LOG IN</Link>}
+            {!user && <Link to='/user/signup'>REGISTRATE</Link>}
+            {user && <button onClick={handleLogout} to='/'>LOG OUT</button>}
             <Link to='/'>NOSOTROS</Link>
         </nav>
         <Outlet />
