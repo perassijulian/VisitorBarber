90a2b19a6fba690b1b3ad966e2538eb1733fe375
Julian <perassijulian@gmail.com>
2022-05-02 12:15:55 -0300

    worker info registered in state

--- client/src/redux/workerRedux.js
+++ client/src/redux/workerRedux.js
@@ -8,23 +8,6 @@ const initialState = {
   workerInfo: null,
 }
 
-//Register worker
-export const registerWorker = createAsyncThunk('auth/registerWorker', async(workerData, thunkAPI) => {
-  try {
-      //const token = thunkAPI.getState().auth.user.token
-      console.log('pre-register')
-      //return await workerService.registerWorker(workerData)
-  } catch (error) {
-      const message = 
-          (error.response && 
-              error.response.date && 
-              error.response.data.message) || 
-          error.message || 
-          error.toString()
-      return thunkAPI.rejectWithValue(message);
-  }
-})
-
 //Get worker info
 export const getWorkerInfo = createAsyncThunk('auth/getAccount', async (_, thunkAPI) => {
 try {
@@ -45,47 +28,26 @@ const workerSlice = createSlice({
   name: "worker",
   initialState,
   reducers: {
-    reset: (state) => {
-      state.isLoading = false
-      state.isSuccess = false
-      state.isError = false
-      state.message = ''
+    registerWorkerStart: (state) => {
+        state.isLoading = true;
     },
-  },
-  extraReducers: (builder) => {
-      builder
-      //registerWorker
-      .addCase(registerWorker.pending, (state) => {
-          state.isLoading = true
-      })
-      .addCase(registerWorker.fulfilled, (state, action) => {
-          state.isLoading = false
-          state.isSuccess = true
-          state.workerInfo = action.payload
-      })
-      .addCase(registerWorker.rejected, (state, action) => {
-          state.isLoading = false
-          state.isError = true
-          state.message = action.payload
-      })
-      
-      //getWorkerInfo
-      .addCase(getWorkerInfo.pending, (state) => {
-          state.isLoading = true
-      })
-      .addCase(getWorkerInfo.fulfilled, (state, action) => {
-          state.isLoading = false
-          state.isSuccess = true
-          state.workerInfo = action.payload
-      })
-      .addCase(getWorkerInfo.rejected, (state, action) => {
-          state.isLoading = false
-          state.isError = true
-          state.message = action.payload
-      })
-
+    registerWorkerSuccess: (state, action) => {
+        state.isLoading = false;
+        state.isSuccess = true
+        state.workerInfo = action.payload;
+    },
+    registerWorkerFailure: (state, action) => {
+        state.isLoading = false;
+        state.isError = true;
+        state.message = action.payload
+    },
   }
 });
 
-export const { reset } = workerSlice.actions;
+export const {
+    registerWorkerStart,
+    registerWorkerSuccess,
+    registerWorkerFailure,
+
+} = workerSlice.actions;
 export default workerSlice.reducer;
--- client/src/redux/workerService.js
+++ client/src/redux/workerService.js
@@ -1,20 +1,32 @@
 import { userRequest } from "../requestMethods";
+import {
+    registerWorkerStart,
+    registerWorkerSuccess,
+    registerWorkerFailure,
+
+} from './workerRedux';
 
 const API_URL = '/worker';
 
 //Register worker
 export const registerWorker = async (dispatch, workerData) => {
-    const user = JSON.parse(localStorage.getItem("persist:root"))?.user;
-    const currentUser = user && JSON.parse(user).currentUser;
-    const TOKEN = currentUser?.accessToken;
-    const response = await userRequest.post(API_URL + '/signup', workerData, {
-        headers: {'token':TOKEN}});
+    dispatch(registerWorkerStart());
+    
+    try {
+        const user = JSON.parse(localStorage.getItem("persist:root"))?.user;
+        const currentUser = user && JSON.parse(user).currentUser;
+        const TOKEN = currentUser?.accessToken;
+        const response = await userRequest.post(API_URL + '/signup', workerData, {
+            headers: {'token':TOKEN}});
 
-    if (response.data) {
-        localStorage.setItem('workerInfo', JSON.stringify(response.data))
-    };
+        if (response.data) {
+            localStorage.setItem('workerInfo', JSON.stringify(response.data))
+        };
 
-    return response.data;
+        dispatch(registerWorkerSuccess(response.data))
+    } catch (error) {
+        dispatch(registerWorkerFailure(error))
+    }
 }
 
 //Get worker info
